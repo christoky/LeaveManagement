5c8c35785d50e83ef356d323ee9b4b85f1b098e8
ChrisToky <rezacolomb@gmail.com>
2026-06-12 11:49:06 -0400

    Add project files.

---
+++ LeaveManagement/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagement.Controllers
+{
+    public class TestController : Controller
+    {       
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DateOfBirth = new DateTime(2000,9,25)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagement/Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagement.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
