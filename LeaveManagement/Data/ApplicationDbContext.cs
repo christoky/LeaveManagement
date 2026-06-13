d48ac500f032e083a2a7b70e3b38ca61514b8ea4
ChrisToky <rezacolomb@gmail.com>
2026-06-13 18:50:52 -0400

    completed Leave Management Module

--- LeaveManagement/Data/ApplicationDbContext.cs
+++ LeaveManagement/Data/ApplicationDbContext.cs
@@ -9,5 +9,7 @@ namespace LeaveManagement.Data
             : base(options)
         {
         }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
     }
 }
---
+++ LeaveManagement/Data/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(150)")]
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+    }
+}
